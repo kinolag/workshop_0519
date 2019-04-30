2e7d1a8f3d33b65f6cafd937365ac43a60346c7e
Nicola <ngrassini@yahoo.it>
2019-04-30 13:16:41 +0100

    Updates

---
+++ precache-manifest.c8014b03e47865dda16d201be0eb6524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fca6891f3d3fdcdfb30aaaaf26201b",
+    "url": "/portfolio_0519/index.html"
+  },
+  {
+    "revision": "e882ba8b13e4480f9645",
+    "url": "/portfolio_0519/static/css/main.3f542169.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/portfolio_0519/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e882ba8b13e4480f9645",
+    "url": "/portfolio_0519/static/js/main.00fcfc8d.chunk.js"
+  },
+  {
+    "revision": "370cb003c8c9ce154378",
+    "url": "/portfolio_0519/static/js/runtime~main.4b752059.js"
+  },
+  {
+    "revision": "f16fd25e1d86638a24000336571a4f43",
+    "url": "/portfolio_0519/static/media/ball_tras_640_i.f16fd25e.png"
+  }
+]);
