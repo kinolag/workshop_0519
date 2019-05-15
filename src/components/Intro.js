e7303fe56f460b498a9dffd1232364a5bb00604c
Nicola <ngrassini@yahoo.it>
2019-05-15 10:07:51 +0100

    audio tag addition

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,13 @@ export default class Intro extends React.Component {
     render() {
         return(
         <>
-            <h3 className='is-white'>Basic <s>portfolio</s> workshop page</h3>
+            <h3 className='is-orange'>Basic <s>portfolio</s> workshop page</h3>
             {/* <h5 className='is-white'>developed in React.js</h5> */}
+            <h5 className="is-white">Connect with the {'<audio>'} tag</h5>
+            <audio className="pad-10 mar-10" controls>
+            <source src="http://www.fotonix.it/temp/dial-up-sound.mp3" type="audio/mpeg" />
+            Your browser does not support the HTML5 audio tag.
+            </audio>
         </>
         );
     }
