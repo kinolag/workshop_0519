24f59f40141b44db1d00d1626630b540330c7bd8
Nicola <ngrassini@yahoo.it>
2019-06-07 21:48:09 +0100

    Header update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ export default class Intro extends React.Component {
     render() {
         return(
         <>
-            <h3 className='is-orange'>Workshop resources | <a href='http://www.nicolag.it/em001.html' className="A2n" title="workshop home">part 1</a></h3>
+            <h4 className='is-orange'>Workshop examples | <a href='http://www.nicolag.it/em001.html' className="A2n" title="Workshop links">Workshop links</a></h4>
             {/* <h5 className='is-white'>developed in React.js</h5> */}
             <h5 className="is-white">Connect with the {'<audio>'} tag</h5>
             <audio className="pad-10 mar-10" controls>
